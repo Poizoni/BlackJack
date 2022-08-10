4daaefd89870450f7186255e753d47a3961d219c
poizoni <wkphillips11@gmail.com>
2022-08-10 15:36:49 -0400

    deal logic completed and working on gameplay functions

--- src/BlackJack.java
+++ src/BlackJack.java
@@ -3,6 +3,8 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Random;
 
 public class BlackJack implements ActionListener {
@@ -13,17 +15,20 @@ public class BlackJack implements ActionListener {
     ArrayList<JLabel> deck;
 
     JLabel playerScoreLabel;
-    int playerScore = 18;
+    int playerScore = 0;
     JLabel dealerScoreLabel;
-    int dealerScore = 16;
+    int dealerScore = 0;
 
     JButton hitButton;
     JButton standButton;
     JButton dealButton;
 
+    HashMap<Integer, Integer> cardValues = new HashMap<>();
+    ArrayList<Integer> numList = new ArrayList<Integer>(51);
+
     Random random;
 
-    final int CARD_AMOUNT = 52;
+    final int CARD_AMOUNT = 51;
 
     Color   btnColor = new Color(34,37,45),
             fieldColor = new Color(39,43,51),
@@ -33,20 +38,66 @@ public class BlackJack implements ActionListener {
             clickColor = new Color(48, 49, 54);
 
     BlackJack() {
+        random = new Random();
+
+        for(int i=0; i<51;i++) {
+            numList.add(i);
+        }
+        Collections.shuffle(numList);
+
+        cardValues.put(0,2);    cardValues.put(1,3);    cardValues.put(2,4);
+        cardValues.put(3,5);    cardValues.put(4,6);    cardValues.put(5,7);
+        cardValues.put(6,8);    cardValues.put(7,9);    cardValues.put(8,10);
+
+        cardValues.put(9,2);    cardValues.put(10,3);   cardValues.put(11,4);
+        cardValues.put(12,5);   cardValues.put(13,6);   cardValues.put(14,7);
+        cardValues.put(15,8);   cardValues.put(16,9);   cardValues.put(17,10);
+
+        cardValues.put(18,2);   cardValues.put(19,3);   cardValues.put(20,4);
+        cardValues.put(21,5);   cardValues.put(22,6);   cardValues.put(23,7);
+        cardValues.put(24,8);   cardValues.put(25,9);   cardValues.put(26,10);
+
+        cardValues.put(27,2);   cardValues.put(28,3);   cardValues.put(29,4);
+        cardValues.put(30,5);   cardValues.put(31,6);   cardValues.put(32,7);
+        cardValues.put(33,8);   cardValues.put(34,9);   cardValues.put(35,10);
+
+        cardValues.put(36,11);   cardValues.put(37,11);   cardValues.put(38,11);   cardValues.put(39,11);
+        cardValues.put(40,10);   cardValues.put(41,10);   cardValues.put(42,10);   cardValues.put(43,10);
+        cardValues.put(44,10);   cardValues.put(45,10);   cardValues.put(46,10);   cardValues.put(47,10);
+        cardValues.put(48,10);   cardValues.put(49,10);   cardValues.put(50,10);   cardValues.put(51,10);
+
 
         deck = new ArrayList<>(51);
-        for(int i=2 ; i<10 ; i++) {
+
+        for(int i=2 ; i<11 ; i++) {
             deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
         }
-        for(int i=2 ; i<10 ; i++) {
+        for(int i=2 ; i<11 ; i++) {
             deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_spades.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
         }
-        for(int i=2 ; i<10 ; i++) {
-            deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        for(int i=2 ; i<11 ; i++) {
+            deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_hearts.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
         }
-        for(int i=2 ; i<10 ; i++) {
-            deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        for(int i=2 ; i<11 ; i++) {
+            deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/"+i+"_of_diamonds.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
         }
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/ace_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/ace_of_diamonds.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/ace_of_hearts.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/ace_of_spades.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/jack_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/jack_of_diamonds.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/jack_of_hearts.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/jack_of_spades.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/king_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/king_of_diamonds.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/king_of_hearts.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/king_of_spades.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/queen_of_clubs.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/queen_of_diamonds.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/queen_of_hearts.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+        deck.add(new JLabel(new ImageIcon (new ImageIcon("src/cards/queen_of_spades.png").getImage().getScaledInstance(130, 200, Image.SCALE_DEFAULT))));
+
 
         frame = new JFrame("BlackJack");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -76,6 +127,7 @@ public class BlackJack implements ActionListener {
         hitButton.setForeground(textColor);
         hitButton.setBounds(135,570,130,80);
         hitButton.setBorder(BorderFactory.createEmptyBorder());
+        hitButton.setVisible(false);
 
         standButton = new JButton("stand");
         standButton.addActionListener(this);
@@ -85,6 +137,7 @@ public class BlackJack implements ActionListener {
         standButton.setForeground(textColor);
         standButton.setBounds(720,570,130,80);
         standButton.setBorder(BorderFactory.createEmptyBorder());
+        standButton.setVisible(false);
 
         dealButton = new JButton("deal");
         dealButton.addActionListener(this);
@@ -120,5 +173,69 @@ public class BlackJack implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        if(e.getSource()==dealButton) {
+            playerPanel.add(deck.get(numList.get(0)));
+            playerPanel.add(deck.get(numList.get(1)));
+
+            playerScore += cardValues.get(numList.get(0));
+            playerScore += cardValues.get(numList.get(1));
+            playerScoreLabel.setText(playerScore + "  player");
+            playerScoreLabel.revalidate();
+            playerScoreLabel.repaint();
+            playerPanel.revalidate();
+            playerPanel.repaint();
+
+            dealerPanel.add(deck.get(numList.get(2)));
+            dealerPanel.add(deck.get(numList.get(3)));
+
+            dealerScore += cardValues.get(numList.get(2));
+            dealerScore += cardValues.get(numList.get(3));
+            dealerScoreLabel.setText("dealer  " + dealerScore);
+            dealerScoreLabel.revalidate();
+            dealerScoreLabel.repaint();
+            dealerPanel.revalidate();
+            dealerPanel.repaint();
+
+            dealButton.setVisible(false);
+            hitButton.setVisible(true);
+            standButton.setVisible(true);
+
+            checkBust();
+
+            frame.revalidate();
+            frame.repaint();
+        }
+        if(e.getSource()==hitButton) {
+
+            if(deck.get(numList.get(4)).getParent() == playerPanel) {
+                playerPanel.add(deck.get(numList.get(5)));
+                playerScore += cardValues.get(numList.get(5));
+            }
+
+            playerPanel.add(deck.get(numList.get(4)));
+            playerScore += cardValues.get(numList.get(4));
+
+            playerScoreLabel.setText(playerScore + "  player");
+
+            checkBust();
+
+            playerScoreLabel.revalidate();
+            playerScoreLabel.repaint();
+            playerPanel.revalidate();
+            playerPanel.repaint();
+
+        }
+        if(e.getSource()==standButton) {
+
+        }
+
+    }
+    public void checkBust() {
+        if(playerScore>21) {
+            System.out.println("player bust");
+        }
+        if(dealerScore>21) {
+            System.out.println("dealer bust");
+        }
     }
 }
